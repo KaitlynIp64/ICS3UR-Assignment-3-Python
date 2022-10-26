91620d9ec4c4cfaec6f79636a046b107675548ab
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-10-26 02:47:40 +0000

    fixed code

--- attendance.py
+++ attendance.py
@@ -9,7 +9,7 @@ def main():
     # this program calculates attendance
 
     # input
-    print("Attendance will decide whether students can sit in the exam.")
+    print("Attendance will decide whether students can sit in the exam or not.")
     classes_held = int(input("Enter the number of classes held: "))
     classes_attended = int(input("Enter the number of classes attended: "))
 
@@ -17,28 +17,20 @@ def main():
 
     attendance_percent = classes_attended / classes_held * 100
 
-    try:
-        classes_held = int(classes_held)
-        classes_attended = int(classes_attended)
-        print("You entered everything correctly.")
-
-    except ValueError:
-        print("That was not a valid number.")
-
     if attendance_percent > 75:
-        print(
-            "The student has {0}% attendance. They may sit in for the exam.".format(
+            print(
+                "The student has {0}% attendance. They may sit in for the exam.".format(
                 attendance_percent
             )
         )
     elif attendance_percent < 75:
-        print(
-            "The student has {0}% attendance. They may not sit in for the exam.".format(
-                attendance_percent
+            print(
+                "The student has {0}% attendance. They may not sit in for the exam.".format(
+                    attendance_percent
             )
         )
     else:
-        print("Input is invalid.".format(attendance_percent))
+            print("Input is invalid.".format(attendance_percent))
 
     # output
     print("\nDone.")
